f32f6ba59604503ad073eab52bcf2dee38f22d35
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-07-15 20:09:07 +0530

    Update main.dart

--- BasicApp/main.dart
+++ BasicApp/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp());                            //runApp is the funtion in the material package of flutter which initiates the running of application.
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {         //we have to extend either stateful or statless widget to any class used in flutter
   void answers() {
     print('Answers');
   }
@@ -18,19 +18,19 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
+        appBar: AppBar(                       //appbar is the head of the app which normally contains the heading 
           title: Text('Hello User'),
         ),
         body: Column(
           children: [
             Text('The Questions are here'),
-            RaisedButton(
+            RaisedButton(                   //raider button is a type of button which can be pressed
               child: Text('Answer 1'),
-              onPressed: answers,
-            ),
+              onPressed: answers,           //onPressed takes a method as an input. Note- when passing the method you have to pass it
+            ),                              // without paranthesis. therfore the method answers is without paranthesis.
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answers,
+              onPressed: () => print('Answer 2'),    //This is another way to pass a single lined method
             ),
             RaisedButton(
               child: Text('Answer 3'),
